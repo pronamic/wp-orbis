a11c7b13b238d1e2197c50ed96146a93ecc93fd5
Remco Tolsma <info@remcotolsma.nl>
2012-08-21 19:21:15 +0200

    WordPress Coding Standards optimizations.

--- admin/meta-box-domain-name-keychains.php
+++ admin/meta-box-domain-name-keychains.php
@@ -1,15 +1,15 @@
 <?php 
 
-wp_nonce_field('orbis_save_domain_name_keychains', 'orbis_domain_name_keychains_meta_box_nonce');
+wp_nonce_field( 'orbis_save_domain_name_keychains', 'orbis_domain_name_keychains_meta_box_nonce' );
 
 ?>
 <table class="form-table">
 	<tr valign="top">
 		<th scope="row">
-			<label for="orbis_domain_name_ftp_keychain_id"><?php _e('FTP', 'orbis') ?></label>
+			<label for="orbis_domain_name_ftp_keychain_id"><?php _e( 'FTP', 'orbis' ) ?></label>
 		</th>
 		<td>
-			<input id="orbis_domain_name_ftp_keychain_id" name="_orbis_domain_name_ftp_keychain_id" value="<?php echo get_post_meta($post->ID, '_orbis_domain_name_ftp_keychain_id', true); ?>" type="text" class="regular-text" />
+			<input id="orbis_domain_name_ftp_keychain_id" name="_orbis_domain_name_ftp_keychain_id" value="<?php echo get_post_meta( $post->ID, '_orbis_domain_name_ftp_keychain_id', true ); ?>" type="text" class="regular-text" />
 		</td>
 	</tr>
 	<tr valign="top">
@@ -17,7 +17,7 @@ wp_nonce_field('orbis_save_domain_name_keychains', 'orbis_domain_name_keychains_
 			<label for="orbis_domain_name_google_apps_keychain_id"><?php _e('Google Apps', 'orbis') ?></label>
 		</th>
 		<td>
-			<input id="orbis_domain_name_google_apps_keychain_id" name="_orbis_domain_name_google_apps_keychain_id" value="<?php echo get_post_meta($post->ID, '_orbis_domain_name_google_apps_keychain_id', true); ?>" type="text" class="regular-text" />
+			<input id="orbis_domain_name_google_apps_keychain_id" name="_orbis_domain_name_google_apps_keychain_id" value="<?php echo get_post_meta( $post->ID, '_orbis_domain_name_google_apps_keychain_id', true ); ?>" type="text" class="regular-text" />
 		</td>
 	</tr>
 	<tr valign="top">
@@ -25,7 +25,7 @@ wp_nonce_field('orbis_save_domain_name_keychains', 'orbis_domain_name_keychains_
 			<label for="orbis_domain_name_wordpress_keychain_id"><?php _e('WordPress', 'orbis') ?></label>
 		</th>
 		<td>
-			<input id="orbis_domain_name_wordpress_keychain_id" name="_orbis_domain_name_wordpress_keychain_id" value="<?php echo get_post_meta($post->ID, '_orbis_domain_name_wordpress_keychain_id', true); ?>" type="text" class="regular-text" />
+			<input id="orbis_domain_name_wordpress_keychain_id" name="_orbis_domain_name_wordpress_keychain_id" value="<?php echo get_post_meta( $post->ID, '_orbis_domain_name_wordpress_keychain_id', true ); ?>" type="text" class="regular-text" />
 		</td>
 	</tr>
 </table>
