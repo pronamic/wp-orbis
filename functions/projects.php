8c956c4ebcba3865333b43d6cc66fc97bf29dcd9
Remco Tolsma <info@remcotolsma.nl>
2012-09-24 15:20:04 +0200

    Added global post to template functions.

--- functions/projects.php
+++ functions/projects.php
@@ -90,20 +90,24 @@ add_filter( 'post_class', 'orbis_projects_post_class' );
 
 
 function orbis_project_is_finished() {
+	global $post;
+
 	$is_finished = false;
 
 	if ( isset( $post->project_is_finished ) ) {
-		$is_finished = $post->project_is_finished;
+		$is_finished = (boolean) $post->project_is_finished;
 	}
 	
 	return $is_finished;
 }
 
 function orbis_project_is_invoiced() {
+	global $post;
+
 	$is_invoiced = false;
 
 	if ( isset( $post->project_is_invoiced ) ) {
-		$is_invoiced = $post->project_is_invoiced;
+		$is_invoiced = (boolean) $post->project_is_invoiced;
 	}
 	
 	return $is_invoiced;
