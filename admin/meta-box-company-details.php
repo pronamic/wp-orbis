27b15ad2a354a0b22d77747aac2c49ae95d03b08
Remco Tolsma <info@remcotolsma.nl>
2013-04-23 09:33:04 +0200

    Added some spacing.

--- admin/meta-box-company-details.php
+++ admin/meta-box-company-details.php
@@ -48,8 +48,12 @@ wp_nonce_field( 'orbis_save_company_details', 'orbis_company_details_meta_box_no
 			</td>
 		</tr>
 		<tr>
-			<th scope="row"><label for="orbis_company_ebilling"><?php _e( 'Ebilling?', 'orbis' ); ?></label></th>
-			<td><input id="orbis_company_ebilling" name="_orbis_company_ebilling" value="1" type="checkbox" <?php if ( (bool) $ebilling ) : ?> checked="checked" <?php endif; ?>/></td>
+			<th scope="row">
+				<label for="orbis_company_ebilling"><?php _e( 'Ebilling?', 'orbis' ); ?></label>
+			</th>
+			<td>
+				<input id="orbis_company_ebilling" name="_orbis_company_ebilling" value="1" type="checkbox" <?php if ( (bool) $ebilling ) : ?> checked="checked" <?php endif; ?>/>
+			</td>
 		</tr>
 	</tbody>
 </table>
