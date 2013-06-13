55b6e3c6853ff1c61b6c5e591996b70e0d44eea5
Remco Tolsma <info@remcotolsma.nl>
2013-06-13 11:36:16 +0200

    Added an shortcode for projects to invoice.

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -49,3 +49,28 @@ function orbis_shortcode_projects_without_agreement( $atts ) {
 }
 
 add_shortcode( 'orbis_projects_without_agreement', 'orbis_shortcode_projects_without_agreement' );
+
+/**
+ * Projects to invoice
+ * 
+ * @param array $atts
+ * @return string
+ */
+function orbis_shortcode_projects_to_invoice( $atts ) {
+	global $wpdb;
+	global $orbis_plugin;
+
+	$return  = '';
+
+	ob_start();
+	
+	$orbis_plugin->plugin_include( 'templates/projects-to-invoice.php' );
+	
+	$return = ob_get_contents();
+	
+	ob_end_clean();
+	
+	return $return;
+}
+
+add_shortcode( 'orbis_projects_to_invoice', 'orbis_shortcode_projects_to_invoice' );
