4d3305946bea54d8bc0f2e61b3794dfb4d9cad3d
Remco Tolsma <info@remcotolsma.nl>
2013-12-09 12:08:38 +0100

    Added an Orbis parse time function.

--- includes/functions.php
+++ includes/functions.php
@@ -1,20 +1,24 @@
 <?php
 
 function orbis_filter_time_input( $type, $variable_name ) {
-	$seconds = 0;
-
 	$value = filter_input( $type, $variable_name, FILTER_SANITIZE_STRING );
+
+	return orbis_parse_time( $value );
+}
+
+function orbis_parse_time( $value ) {
+	$seconds = 0;
 
 	$part_hours   = $value;
 	$part_minutes = 0;
-
+	
 	$position_colon = strpos( $value, ':' );
-
+	
 	if ( $position_colon !== false ) {
 		$part_hours   = substr( $value, 0, $position_colon );
 		$part_minutes = substr( $value, $position_colon + 1 );
 	}
-
+	
 	$var = filter_var( $part_hours, FILTER_VALIDATE_FLOAT );
 	if ( $var !== false ) {
 		$seconds += $var * 3600;
@@ -24,7 +28,7 @@ function orbis_filter_time_input( $type, $variable_name ) {
 	if ( $var !== false ) {
 		$seconds += $var * 60;
 	}
-
+	
 	return $seconds;
 }
 
