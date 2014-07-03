17cce28e50f4eb15a7bb187648406c7f42ac8f59
rclations <rc@hallme.com>
2014-07-03 09:25:16 -0400

    Removed deprecated screen_icon() function calls
    see http://codex.wordpress.org/Function_Reference/screen_icon

--- views/orbis.php
+++ views/orbis.php
@@ -1,5 +1,4 @@
 <div class="wrap">
-	<?php screen_icon( 'orbis' ); ?>
 
 	<h2><?php echo get_admin_page_title(); ?></h2>
 
--- views/settings.php
+++ views/settings.php
@@ -1,7 +1,21 @@
 <div class="wrap">
-	<?php screen_icon( 'orbis' ); ?>
 
 	<h2>
 		<?php _e( 'Settings', 'orbis' ); ?>
 	</h2>
+</div>
+
+
+<div class="wrap">
+
+	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+
+	<!-- @TODO: Provide markup for your options page here. -->
+	
+    <form action="options.php" method="POST">
+        <?php settings_fields( 'wp_whitelabel' ); ?>
+        <?php do_settings_sections( 'wp_whitelabel' ); ?>
+        <?php submit_button(); ?>
+    </form>
+    
 </div>
