d1c26596866b0f79a2ea2e898e645d563a47c0a2
Remco Tolsma <info@remcotolsma.nl>
2014-07-04 09:35:20 +0200

    Render Orbis settings fields and sections.

--- admin/page-settings.php
+++ admin/page-settings.php
@@ -1,5 +1,11 @@
 <div class="wrap">
 	<h2><?php echo get_admin_page_title(); ?></h2>
 
+	<form action="options.php" method="POST">
+		<?php settings_fields( 'orbis' ); ?>
 
+		<?php do_settings_sections( 'orbis' ); ?>
+
+		<?php submit_button(); ?>
+	</form>
 </div>
