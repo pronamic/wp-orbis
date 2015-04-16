68d498fa6a7fd0852dff4d372755ce9e0a9ebcd2
Remco Tolsma <info@remcotolsma.nl>
2015-04-16 12:02:11 +0200

    Added support for company category so we can categorize companies.

--- includes/post.php
+++ includes/post.php
@@ -89,6 +89,32 @@ function orbis_create_initial_post_types() {
 		)
 	);
 
+	register_taxonomy(
+		'orbis_company_category',
+		array( 'orbis_company' ),
+		array(
+			'hierarchical' => true,
+			'labels'       => array(
+				'name'              => _x( 'Categories', 'taxonomy general name', 'orbis' ),
+				'singular_name'     => _x( 'Category', 'taxonomy singular name', 'orbis' ),
+				'search_items'      => __( 'Search Categories', 'orbis' ),
+				'all_items'         => __( 'All Categories', 'orbis' ),
+				'parent_item'       => __( 'Parent Category', 'orbis' ),
+				'parent_item_colon' => __( 'Parent Category:', 'orbis' ),
+				'edit_item'         => __( 'Edit Category', 'orbis' ),
+				'update_item'       => __( 'Update Category', 'orbis' ),
+				'add_new_item'      => __( 'Add New Category', 'orbis' ),
+				'new_item_name'     => __( 'New Category Name', 'orbis' ),
+				'menu_name'         => __( 'Categories', 'orbis' ),
+			),
+			'show_ui'      => true,
+			'query_var'    => true,
+			'rewrite'      => array(
+				'slug' => _x( 'company-category', 'slug', 'orbis' ),
+			),
+		)
+	);
+
 	register_post_type(
 		'orbis_person' ,
 		array(
