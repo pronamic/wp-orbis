0899cb452a4e204ba541a01b8274e69db85fad81
Remco Tolsma <info@remcotolsma.nl>
2015-06-18 15:37:06 +0200

    Added keychains to daily update mail.

--- templates/emails/update.php
+++ templates/emails/update.php
@@ -326,6 +326,49 @@ $defaults = array(
 </div>
 
 <div>
+	<h2><?php esc_html_e( 'Recent Keychains', 'orbis' ); ?></h2>
+
+	<?php
+
+	$query = new WP_Query( wp_parse_args( array( 'post_type' => 'orbis_keychain' ), $defaults ) );
+
+	if ( $query->have_posts() ) : ?>
+
+		<table style="<?php echo esc_attr( $table_style ); ?>" cellpadding="<?php echo esc_attr( $table_padding ); ?>">
+			<thead>
+				<tr>
+					<th scope="col">
+						<?php esc_html_e( 'Keychain', 'orbis' ); ?>
+					</th>
+				</tr>
+			</thead>
+
+			<tbody>
+
+				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+
+					<tr>
+						<td>
+							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br />
+						</td>
+					</tr>
+
+				<?php endwhile; ?>
+
+			</tbody>
+		</table>
+
+	<?php else : ?>
+
+		<p>
+			<em><?php esc_html_e( 'No new posts in the last 3 days.', 'orbis' ); ?></em>
+		</p>
+
+	<?php endif; ?>
+
+</div>
+
+<div>
 	<h2><?php esc_html_e( 'Recent Tasks', 'orbis' ); ?></h2>
 
 	<?php
