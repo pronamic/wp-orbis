5246aa9f075f5b4724e142d58c9b29be8584ef42
Remco Tolsma <info@remcotolsma.nl>
2015-06-18 15:36:30 +0200

    Added recent posts block to the daily update mail.

--- templates/emails/update.php
+++ templates/emails/update.php
@@ -20,6 +20,55 @@ $defaults = array(
 <?php do_action( 'orbis_email_top' ); ?>
 
 <div>
+	<h2><?php esc_html_e( 'Recent Posts', 'orbis' ); ?></h2>
+
+	<?php
+
+	$query = new WP_Query( wp_parse_args( array( 'post_type' => 'post' ), $defaults ) );
+
+	if ( $query->have_posts() ) : ?>
+
+		<table style="<?php echo esc_attr( $table_style ); ?>" cellpadding="<?php echo esc_attr( $table_padding ); ?>">
+			<thead>
+				<tr>
+					<th scope="col">
+						<?php esc_html_e( 'Date', 'orbis' ); ?>
+					</th>
+					<th scope="col">
+						<?php esc_html_e( 'Name', 'orbis' ); ?>
+					</th>
+				</tr>
+			</thead>
+
+			<tbody>
+
+				<?php while ( $query->have_posts() ) : $query->the_post(); ?>
+
+					<tr>
+						<td>
+							<?php the_time( 'D j M' ); ?>
+						</td>
+						<td>
+							<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br />
+						</td>
+					</tr>
+
+				<?php endwhile; ?>
+
+			</tbody>
+		</table>
+
+	<?php else : ?>
+
+		<p>
+			<em><?php esc_html_e( 'No new posts in the last 3 days.', 'orbis' ); ?></em>
+		</p>
+
+	<?php endif; ?>
+
+</div>
+
+<div>
 	<h2><?php esc_html_e( 'Recent Companies', 'orbis' ); ?></h2>
 
 	<?php
