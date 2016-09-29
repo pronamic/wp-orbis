9f3f7d876dba48fddfbd39c01179181ae16bcd53
I <info@remcotolsma.nl>
2016-09-29 15:21:42 +0200

    Added latest comment to the projects table view.

--- templates/projects-table-view.php
+++ templates/projects-table-view.php
@@ -7,6 +7,7 @@
 				<th scope="col"><?php _e( 'Client', 'orbis' ); ?></th>
 				<th scope="col"><?php _e( 'Project', 'orbis' ); ?></th>
 				<th scope="col"><?php _e( 'Date', 'orbis' ); ?></th>
+				<th scope="col"><?php _e( 'Comment', 'orbis' ); ?></th>
 				<th scope="col"><?php _e( 'Time', 'orbis' ); ?></th>
 				<th scope="col"><?php _e( 'Invoiceable', 'orbis' ); ?></th>
 				<th scope="col"><?php _e( 'Invoice Number', 'orbis' ); ?></th>
@@ -45,6 +46,34 @@
 							<?php echo esc_html( get_the_time( 'j M Y', $project->project_post_id ) ); ?>
 						</td>
 						<td style="white-space: nowrap;">
+							<?php
+
+							$comments_query = new WP_Comment_Query();
+
+							$comments = $comments_query->query( array(
+								'number'  => 1,
+								'post_id' => $project->project_post_id,
+							) );
+
+							foreach ( $comments as $comment ) {
+								$title = sprintf(
+									__( '%s zegt op %s:', 'orbis' ),
+									'<strong>' . $comment->comment_author . '</strong>',
+									'<strong>' . date_i18n( 'j M Y', strtotime( $comment->comment_date ) ) . '</strong>'
+								);
+
+								printf(
+									'<a href="%s" data-container="body" data-trigger="hover" data-toggle="popover" data-placement="right" data-title="%s" data-content="%s" data-html="true" role="button">%s</a>',
+									esc_attr( get_comment_link( $comment ) ),
+									esc_attr( $title ),
+									esc_attr( $comment->comment_content ),
+									date_i18n( 'j M Y', strtotime( $comment->comment_date ) ) . ' <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>'
+								);
+							}
+
+							?>
+						</td>
+						<td style="white-space: nowrap;">
 							<span style="color: <?php echo esc_attr( $project->failed ? 'Red' : 'Green' ); ?>;"><?php echo esc_html( orbis_time( $project->registeredSeconds ) ); ?></span>
 							/
 							<?php echo esc_html( orbis_time( $project->availableSeconds ) ); ?>
@@ -70,3 +99,9 @@
 		</tbody>
 	</table>
 </div>
+
+<script>
+	jQuery( document ).ready( function( $ ) {
+    	$( '[data-toggle="popover"]' ).popover(); 
+	} );
+</script>
