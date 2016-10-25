0d43a7803ebe69cce6b36e8a0e8750d3899b77b0
Remco Tolsma <info@remcotolsma.nl>
2016-10-25 10:22:56 +0200

    Extended Orbis person class with some helper functions to retrieve title and organisation.

--- classes/orbis-person.php
+++ classes/orbis-person.php
@@ -22,6 +22,14 @@ class Orbis_Person {
 		$this->post = get_post( $post );
 	}
 
+	public function get_title() {
+		return get_post_meta( $this->post->ID, '_orbis_title', true );
+	}
+
+	public function get_organization() {
+		return get_post_meta( $this->post->ID, '_orbis_organization', true );
+	}
+
 	public function get_email() {
 		return get_post_meta( $this->post->ID, '_orbis_person_email_address', true );
 	}
@@ -32,5 +40,56 @@ class Orbis_Person {
 		$gender = is_array( $genders ) ? reset( $genders ) : null;
 
 		return $gender;
+	}
+
+	public function get_address() {
+		$address = new Orbis_Address();
+		$address->address  = get_post_meta( $this->post->ID, '_orbis_address', true );
+		$address->postcode = get_post_meta( $this->post->ID, '_orbis_postcode', true );
+		$address->city     = get_post_meta( $this->post->ID, '_orbis_city', true );
+		$address->country  = get_post_meta( $this->post->ID, '_orbis_country', true );
+
+		return $address;
+	}
+}
+
+class Orbis_Address {
+	public function get_address() {
+		return $this->address;
+	}
+
+	public function get_postcode() {
+		return $this->postcode;
+	}
+
+	public function get_city() {
+		return $this->city;
+	}
+
+	public function get_country() {
+		return $this->country;
+	}
+
+	public function is_empty() {
+		$data = arra_filter( array(
+			$this->address,
+			$this->postcode,
+			$this->city,
+			$this->country,
+		) );
+
+		return empty( $data );
+	}
+
+	public function __toString() {
+		$data = array(
+			$this->address,
+			trim( $this->postcode . ' ' . $this->city ),
+			$this->country,
+		);
+
+		$data = array_filter( $data );
+
+		return implode( "\r\n", $data );
 	}
 }
