ef57d803f4206c2ac024442a9560b9112f5f9698
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-01-08 10:29:01 +0100

    Update orbis.php

--- orbis.php
+++ orbis.php
@@ -1,27 +1,33 @@
 <?php
-/*
-Plugin Name: Orbis
-Plugin URI: https://www.pronamic.eu/plugins/orbis/
-Description: Orbis is a powerful, extendable plugin to boost up your business. Project Management, Customer Relation Management & More...
-
-Version: 1.3.3
-Requires at least: 3.0
-
-Author: Pronamic
-Author URI: https://www.pronamic.eu/
-
-Text Domain: orbis
-Domain Path: /languages/
-
-License: GPL
-
-GitHub URI: https://github.com/wp-orbis/wp-orbis
-*/
+/**
+ * Orbis
+ *
+ * @package   Pronamic\Orbis
+ * @author    Pronamic
+ * @copyright 2024 Pronamic
+ * @license   GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Orbis
+ * Plugin URI:        https://wp.pronamic.directory/plugins/orbis/
+ * Description:       Orbis is a powerful, extendable plugin to boost up your business. Project Management, Customer Relation Management & More…
+ * Version:           1.3.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Pronamic
+ * Author URI:        https://www.pronamic.eu/
+ * Text Domain:       orbis
+ * Domain Path:       /languages/
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://wp.pronamic.directory/plugins/orbis/
+ * GitHub URI:        https://github.com/pronamic/wp-orbis
+ */
 
 /**
  * Autoload
  */
-require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload_packages.php';
 
 /**
  * Includes
