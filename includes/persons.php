95136fdc48fa351f2a5a57afa58755fba7df7a9f
anupamme <mediratta@gmail.com>
2026-09-16 09:18:54 +0000

    fix: V-001 security vulnerability
    Automated security fix generated by OrbisAI Security

--- includes/persons.php
+++ includes/persons.php
@@ -3,7 +3,7 @@
 function orbis_persons_suggest_person_id() {
 	global $wpdb;
 
-	$term = filter_input( INPUT_GET, 'term', FILTER_UNSAFE_RAW );
+	$term = filter_input( INPUT_GET, 'term', FILTER_SANITIZE_FULL_SPECIAL_CHARS );
 	$term = ( null === $term ) ? '' : sanitize_text_field( wp_unslash( $term ) );
 
 	$query = "
